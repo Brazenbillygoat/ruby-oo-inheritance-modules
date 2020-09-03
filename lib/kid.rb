a39bcafe7d3f5310d79649e5ceb434006eae79b1
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-09-03 15:04:22 -0400

    Added functionality to classes via modules

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,15 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
+class Kid
+  extend FancyDance::ClassMethods #This enables MetaDancing's class methods
+  include FancyDance::InstanceMethods #This enables Dance's instance methods
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
